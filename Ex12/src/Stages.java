3f33fe274c3185784658a35a5203e5135443caaf
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-04-07 23:58:05 -0400

    ok

--- Ex12/src/Stages.java
+++ Ex12/src/Stages.java
@@ -7,5 +7,7 @@ This program asks the user for an age input, and outputs what stage of life
 they would be in that age.
  */
 public class Stages {
-    
+    public static void main(String[] args) {
+
+    }
 }
