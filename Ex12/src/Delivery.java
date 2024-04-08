42eb4c7156e575c9256642a80c5bf2350596d619
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-04-08 00:43:16 -0400

    ok

--- Ex12/src/Delivery.java
+++ Ex12/src/Delivery.java
@@ -1,3 +1,15 @@
+/*
+Program: Stages
+Programmer: Ryan Mehrian
+Date: March 18, 2023
+----
+This program asks the user for the weight and dimensions of a package, and outputs whether the package is eligible
+for delivery and the cost for delivery if it is eligible.
+ */
 public class Delivery {
-
+    public static void main(String[] args) {
+        //Variable Declaration
+        final int MAX_KILO = 27;
+        final int MAX_CM3 = 100 000;
+    }
 }
