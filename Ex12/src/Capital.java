3ca15b2a41a79a1b3bd00964c7a9ee0724552294
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-04-10 10:19:24 -0400

    tes

---
+++ Ex12/src/Capital.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+Program: Capital
+Programmer: Ryan Mehrian
+Date: March 18, 2023
+----
+Program asks user to input capital of Canada, and outputs if they are correct or not.
+ */
+public class Capital {
+    public static void main(String[] args) {
+        //Variables
+        final String ANSWER = "Ottawa";
+        String input;
+
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("What is the capital of Canada? \n > ");
+        input = sc.nextLine();
+
+        //Check and Output
+        if(input.toUpperCase() == ANSWER.toUpperCase())
+            System.out.println("Correct");
+        else
+            System.out.print("Incorrect");
+    }
+}
--- Ex12/src/Delivery.java
+++ Ex12/src/Delivery.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /*
-Program: Stages
+Program: Delivery
 Programmer: Ryan Mehrian
 Date: March 18, 2023
 ----
@@ -18,5 +20,39 @@ public class Delivery {
         final double PRICE_BRACKET2 = 3.50;
         final double PRICE_BRACKET3 = 4.00;
         final double PRICE_BRACKET4 = 4.50;
+        double weight, length, width, height, price = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("Please enter package weight (kg): ");
+        weight = sc.nextDouble();
+        System.out.print("Please enter package width (cm): ");
+        width = sc.nextDouble();
+        System.out.print("Please enter package height (cm): ");
+        height = sc.nextDouble();
+        System.out.print("Please enter package length (cm): ");
+        length = sc.nextDouble();
+
+        //Checks and Outputs
+        if(weight > WEIGHT_BRACKET4){
+            System.out.println("Too Heavy");
+        }
+        else if(length*height*width > MAX_CM3){
+            System.out.println("Too Large");
+        }
+        else{
+            if(weight <= WEIGHT_BRACKET1)
+                price = PRICE_BRACKET1*weight;
+            else if(weight <= WEIGHT_BRACKET2)
+                price = PRICE_BRACKET2*weight;
+            else if(weight <= WEIGHT_BRACKET3)
+                price = PRICE_BRACKET3*weight;
+            else
+                price = PRICE_BRACKET4*weight;
+
+            System.out.printf("$%.2f", price);
+
+        }
     }
 }
