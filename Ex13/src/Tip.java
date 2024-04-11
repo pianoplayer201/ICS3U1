126bfb427cd6a01a23e49ea00816018eb18c9bf9
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-04-11 12:40:30 -0400

    yes

---
+++ Ex13/src/Tip.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+Program: Tip
+Programmer: Ryan Mehrian
+Date: March 28, 2024
+-----
+The program will ask the user to input their letter grade for their ICS course.
+The program will respond with a different study tip for each letter grade.
+ */
+public class Tip {
+    public static void main(String[]args){
+        //Declarations
+        String letterGrade;
+
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("Please enter your letter grade in ICS: ");
+        letterGrade = sc.nextLine();
+
+        //Output
+        switch ()
+    }
+}
