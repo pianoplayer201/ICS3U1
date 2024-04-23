936ccdb80f72d844cc9e6850395de81f34549147
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-04-23 11:37:23 -0400

    yes (please remember to upload exercise 28)

---
+++ Ex31/src/Welcome.java
@@ -0,0 +1,20 @@
+/*
+Program: Welcome
+Programmer: Ryan Mehrian
+Date
+-----
+This program contains a method that will greet a visitor, and let them know what # visitor they are.
+ */
+public class Welcome {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Method: welcomeMessage
+    ------
+    Returns:
+    String - 
+     */
+    public static String welcomeMesssage()
+}
